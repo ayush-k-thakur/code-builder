e58a90568eece4dc03f9b54b9f21c56ecf12ffaa
Ayush Kumar Thakur <ayush524425@gmail.com>
2025-10-07 19:50:51 +0545

    first commit

--- server/index.js
+++ server/index.js
@@ -1,55 +1,101 @@
-// // server/index.js
-// import express from "express";
-// import http from "http";
+// // // server/index.js
+// // import express from "express";
+// // import http from "http";
+// // import { Server } from "socket.io";
+// // import cors from "cors";
+// // import path from "path";
+
+// // const app = express();
+// // app.use(cors({ origin: "*" })); // allow all origins for dev
+// // app.use(express.json());
+
+// // const server = http.createServer(app);
+
+// // const io = new Server(server, {
+// //   cors: { origin: "*" }, // allow React app to connect
+// //   transports: ["websocket", "polling"], // ensures websocket first, fallback polling
+// // });
+
+// // const rooms = {};
+
+// // io.on("connection", (socket) => {
+// //   console.log("User connected:", socket.id);
+
+// //   socket.on("join-room", (roomId) => {
+// //     socket.join(roomId);
+// //     if (rooms[roomId]) {
+// //       socket.emit("sync-code", rooms[roomId]);
+// //     }
+// //   });
+
+// //   socket.on("code-change", ({ roomId, code }) => {
+// //     rooms[roomId] = code;
+// //     socket.to(roomId).emit("code-change", code);
+// //   });
+
+// //   socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+// // });
+
+// // app.use(express.static(path.join(__dirname, "../client/build")));
+
+// // app.get("*", (req, res) => {
+// //   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+// // });
+
+// // server.listen(5000, () => console.log("Server running on port 5000"));
+
+// // api/socket.js
 // import { Server } from "socket.io";
-// import cors from "cors";
-// import path from "path";
 
-// const app = express();
-// app.use(cors({ origin: "*" })); // allow all origins for dev
-// app.use(express.json());
+// let io;
 
-// const server = http.createServer(app);
+// export default function handler(req, res) {
+//   if (!res.socket.server.io) {
+//     console.log("Setting up Socket.io server...");
+//     io = new Server(res.socket.server, {
+//       path: "/api/socket",
+//       cors: {
+//         origin: "*",
+//         methods: ["GET", "POST"],
+//       },
+//       transports: ["websocket", "polling"],
+//     });
 
-// const io = new Server(server, {
-//   cors: { origin: "*" }, // allow React app to connect
-//   transports: ["websocket", "polling"], // ensures websocket first, fallback polling
-// });
+//     const rooms = {};
 
-// const rooms = {};
+//     io.on("connection", (socket) => {
+//       console.log("User connected:", socket.id);
 
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
+//       socket.on("join-room", (roomId) => {
+//         socket.join(roomId);
+//         if (rooms[roomId]) socket.emit("sync-code", rooms[roomId]);
+//       });
 
-//   socket.on("join-room", (roomId) => {
-//     socket.join(roomId);
-//     if (rooms[roomId]) {
-//       socket.emit("sync-code", rooms[roomId]);
-//     }
-//   });
+//       socket.on("code-change", ({ roomId, code }) => {
+//         rooms[roomId] = code;
+//         socket.to(roomId).emit("code-change", code);
+//       });
 
-//   socket.on("code-change", ({ roomId, code }) => {
-//     rooms[roomId] = code;
-//     socket.to(roomId).emit("code-change", code);
-//   });
+//       socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+//     });
 
-//   socket.on("disconnect", () => console.log("User disconnected:", socket.id));
-// });
+//     res.socket.server.io = io;
+//   }
 
-// app.use(express.static(path.join(__dirname, "../client/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-// });
-
-// server.listen(5000, () => console.log("Server running on port 5000"));
-
-// api/socket.js
+//   res.end();
+// }
 import { Server } from "socket.io";
 
 let io;
 
 export default function handler(req, res) {
+  // Handle HTTP GET request to '/'
+  if (req.method === "GET" && req.url === "/") {
+    res.status(200).send("Welcome to the homepage!");
+    return;
+  }
+
+  // Socket.io setup
   if (!res.socket.server.io) {
     console.log("Setting up Socket.io server...");
     io = new Server(res.socket.server, {
@@ -82,5 +128,8 @@ export default function handler(req, res) {
     res.socket.server.io = io;
   }
 
-  res.end();
+  // For non-GET requests, end the response
+  if (req.method !== "GET") {
+    res.end();
+  }
 }
